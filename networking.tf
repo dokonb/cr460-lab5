63f095da418e06ee50e68da6c6060c1be0e6ccaf
DOGNIMÉ KONÉ <dognime.kone@polymtl.ca>
2017-02-25 20:12:40 -0500

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -3,16 +3,16 @@ resource "google_compute_network" "cr460" {
     auto_create_subnetworks = "false"
 }
 
-resource "google_compute_subnetwork" "east1-dmz" {
-    name          = "east1-dmz"
+resource "google_compute_subnetwork" "central1-dmz" {
+    name          = "central1-dmz"
     ip_cidr_range = "172.16.1.0/24"
     network       = "${google_compute_network.cr460.self_link}"
-    region        = "us-east1"
+    region        = "us-central1"
 }
 
-resource "google_compute_subnetwork" "east1-internal" {
-    name          = "east1-internal"
+resource "google_compute_subnetwork" "central1-internal" {
+    name          = "central1-internal"
     ip_cidr_range = "10.0.1.0/24"
     network       = "${google_compute_network.cr460.self_link}"
-    region        = "us-east1"
+    region        = "us-central1"
 }
