789c0d0e14f0e7a150d1702134433d0e2c23e2f8
DOGNIMÉ KONÉ <dognime.kone@polymtl.ca>
2017-02-25 19:50:25 -0500

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -36,7 +36,7 @@ resource "google_compute_firewall" "ssh" {
 }
 
 resource "google_dns_record_set" "www" {
-    name = "beauchef.cr460lab.com."
+    name = "dokonb.cr460lab.com."
     type = "A"
     ttl = 300
     managed_zone = "cr460lab"
