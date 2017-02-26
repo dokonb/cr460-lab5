ad3bd0723f16fe5b11dbd730bfe2b69e73c0ca22
DOGNIMÉ KONÉ <dognime.kone@polymtl.ca>
2017-02-25 19:44:58 -0500

    Create compute.tf

---
+++ compute.tf
@@ -0,0 +1,44 @@
+resource "google_compute_instance" "instance1" {
+    name = "instance1"
+    machine_type = "f1-micro"
+    zone = "us-east1-b"
+
+    disk {
+        image = "debian-cloud/debian-8"
+    }
+
+    network_interface {
+        subnetwork = "${google_compute_subnetwork.east1-dmz.name}"
+        access_config {
+            // IP
+        }
+    }
+
+    metadata_startup_script = "apt-get -y install apache2 && systemctl start apache2"
+}
+
+resource "google_compute_firewall" "web" {
+    name = "web"
+    network = "${google_compute_network.cr460.name}"
+    allow {
+        protocol = "tcp"
+        ports = ["80"]
+    }
+}
+
+resource "google_compute_firewall" "ssh" {
+    name = "ssh"
+    network = "${google_compute_network.cr460.name}"
+    allow {
+        protocol = "tcp"
+        ports = ["22"]
+    }
+}
+
+resource "google_dns_record_set" "www" {
+    name = "beauchef.cr460lab.com."
+    type = "A"
+    ttl = 300
+    managed_zone = "cr460lab"
+    rrdatas = ["${google_compute_instance.instance1.network_interface.0.access_config.0.assigned_nat_ip}"]
+}
